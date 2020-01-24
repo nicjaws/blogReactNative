741841505ec4d1576c369dc361ca1d76f597c9e1
Nicolas Corbalan <outreef@hotmail.com>
2020-01-24 13:10:43 +0100

    add post screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,10 +3,8 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import {Context} from '../context/BlogContext'; 
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const {state, addBlogPost, deleteBlogPost} = useContext(Context);
-
-
 
   return (
   <View>
@@ -15,16 +13,30 @@ const IndexScreen = () => {
       data={state}
       keyExtractor={blogPost => blogPost.title}
       renderItem={({item}) => {
-        return <View style={styles.row}>
-          <Text style={styles.title}>{item.title} - {item.id}</Text>
-          <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-            <Feather style={styles.icon} name="trash-2"/>
-          </TouchableOpacity>
+        return (
+          <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}
+          >
+            <View style={styles.row}>
+            <Text style={styles.title}>{item.title} - {item.id}</Text>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              <Feather style={styles.icon} name="trash-2"/>
+            </TouchableOpacity>
           </View>
+          </TouchableOpacity>
+          )
       }}
     />
   </View>
   )
+};
+
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => 
+    <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+      <Feather name="plus" size={30} />
+    </TouchableOpacity>
+  }
 }
 
 const styles = StyleSheet.create({
@@ -40,7 +52,8 @@ const styles = StyleSheet.create({
     fontSize: 18
   },
   icon: {
-    fontSize: 24
+    fontSize: 24,
+    color: 'red'
   }
 })
 
